563cc8a48327e2797a9b085cf93fa64e3fd27853
Quentinmac mini <bm.quentin@icloud.com>
2023-02-03 04:21:44 +0100

    a

--- vm-main.sh
+++ vm-main.sh
@@ -15,7 +15,7 @@ echo "3. Redemarer VM"
 echo "4. Eteindre VM"
 read vmAction
 
-if [ "$vmAction" == "1"]; then
+if [ "$vmAction" == "1" ]; then
     ramDispo=$(free -m | grep Mem | awk '{print $7}')
     echo "Entrer nouvelle valeur de RAM souhaité en MB"
     read ram
@@ -28,11 +28,11 @@ if [ "$vmAction" == "1"]; then
         echo "Juste pour information il reste $ramDispo MB"
         echo "Maintenant tu a le drois de pleurer et recommancer ta modif. Aller Bis :)"
     fi
-elif [ "$vmAction" == "2"]; then
+elif [ "$vmAction" == "2" ]; then
     echo "a venir"
-elif [ "$vmAction" == "3"]; then
+elif [ "$vmAction" == "3" ]; then
     echo "a venir"
-elif [ "$vmAction" == "4"]; then
+elif [ "$vmAction" == "4" ]; then
     echo "a venir"
 else
     echo "Tu te crois malin a pas entrer un chiffre ?"
