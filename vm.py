47e5e7c08d461f10d0a36bc38507dea561091b79
Quentinmac mini <bm.quentin@icloud.com>
2023-02-03 05:27:16 +0100

    a

--- vm.py
+++ vm.py
@@ -1,6 +1,6 @@
 import proxmoxer
 
-proxmox = proxmoxer.ProxmoxAPI('192.168.1.10', user='root', password='Betarr0103.', verify_ssl=False)
+proxmox = proxmoxer.ProxmoxAPI('192.168.1.10', user='root@pam', password='Betarr0103.', verify_ssl=False)
 
 
 print("Entrer l'id de la vm a modifier")
