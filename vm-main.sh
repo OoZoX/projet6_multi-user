15babe3bee60b8861c325a0b2495a3b1a79eed18
Quentinmac mini <bm.quentin@icloud.com>
2023-02-03 04:47:07 +0100

    a

--- vm-main.sh
+++ vm-main.sh
@@ -19,14 +19,14 @@ echo "4. Eteindre VM"
 read -p "Choix : " vmAction
 
 if [ "$vmAction" == "1" ]; then
-    vmIdInt=$((10#vmId))
-    echo "$vmIDint"
+    let vmIdInt=vmId
+    echo $vmIDint
     ramDispo=$(free -m | grep Mem | awk '{print $7}')
     echo "Entrer nouvelle valeur de RAM souhaité en MB"
     echo "Pour information il reste : $ramDispo MB"
     
     read ram
-    ramInt=$((10#ram))
+    let ramInt=ram
     
     if [ $ramInt -le $ramDispo ]; then
         qm set vmIDInt --memory $ramInt
